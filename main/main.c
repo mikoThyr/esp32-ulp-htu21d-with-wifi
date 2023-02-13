61511f93f94bd324c232d855ea96166f29f98684
mikoThyr <mikolajczyk.edu@gmail.com>
2023-02-13 20:06:52 +0100

    first commit

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "esp32/ulp.h"
+
+
+extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
+extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
+
+void app_main(void)
+{
+
+}
